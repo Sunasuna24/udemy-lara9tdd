37edbb59a7a4f8931eb6ff40b85ca046e1994ad2
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-08 02:47:22 +0900

    ブラウザにブログのタイトルが表示されるようにした

--- app/Http/Controllers/PostListController.php
+++ app/Http/Controllers/PostListController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostListController extends Controller
 {
     public function index()
     {
-        //
+        $posts = Post::all();
+
+        return view('index')->with('posts', $posts);
     }
 }
