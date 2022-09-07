1850713807510b6961d68169b090721e8be201a3
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-08 02:48:03 +0900

    ブログのタイトルが2つ表示されるのを確認した

--- tests/Feature/Http/Controllers/PostListControllerTest.php
+++ tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -2,15 +2,24 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostListControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     function TOPページでブログ一覧が表示される()
     {
-        $this->get('/')->assertOk();
+        $post1 = Post::factory()->create();
+        $post2 = Post::factory()->create();
+
+        $this->get('/')
+              ->assertOk()
+              ->assertSee($post1->title)
+              ->assertSee($post2->title);
     }
 }
