1200e59eb40bc54b1656dbd7dcdc9e3a2a94fe7b
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-10 05:52:29 +0900

    取得したブログのステータスが非公開のときは403abortする

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,6 +16,9 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
+        if ($post->status === Post::CLOSED) {
+            abort(403);
+        }
         return view('posts.show')->with('post', $post);
     }
 }
