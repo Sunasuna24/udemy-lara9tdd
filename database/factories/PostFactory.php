47905abda4bab01c42052045fdb331cbbdc38803
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-10 02:07:41 +0900

    Postモデルにuser_idを含めた

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,6 +18,11 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
+            'user_id' => User::factory(),
+            // 上記の記法は下記の記法のショートカット
+            // 'user_id' => function () {
+            //     return User::factory()->create()->id;
+            // },
             'title' => $this->faker->realText(20),
             'body' => $this->faker->realText(200)
         ];
