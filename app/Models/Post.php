fd53d888813f7624535a5a909aea30bc92ce0727
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-10 02:44:09 +0900

    投稿がcommentリレーションをコレクションで返すのを確認した

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -13,5 +14,10 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Models;
 
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -17,5 +18,12 @@ class PostTest extends TestCase
     {
         $post = Post::factory()->create();
         $this->assertInstanceOf(User::class, $post->user);
+    }
+
+    /** @test */
+    function commentsリレーションを返す()
+    {
+        $post = Post::factory()->create();
+        $this->assertInstanceOf(Collection::class, $post->comments);
     }
 }
