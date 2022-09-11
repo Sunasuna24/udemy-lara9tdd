b5eb6e0b8b546c95ac8c0125a0bd51c9d1dc6bc0
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-11 10:23:58 +0900

    会員登録されるのを確認した

--- tests/Feature/Http/Controllers/SignUpControllerTest.php
+++ tests/Feature/Http/Controllers/SignUpControllerTest.php
@@ -8,10 +8,30 @@ use Tests\TestCase;
 
 class SignUpControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     function 会員登録画面が表示される()
     {
         $this->get('signup')
              ->assertOk();
+    }
+
+    /** @test */
+    function 会員登録できる()
+    {
+        // データ検証
+        $validData = [
+            'name' => 'ユーザー太郎',
+            'email' => 'test1@email.com',
+            'password' => 'password'
+        ];
+
+        $this->post('signup', $validData)
+              ->assertOk();
+
+        // データが保存されているかを検証する
+        unset($validData['password']);  // ハッシュ化されて検証に使えないから除く
+        $this->assertDatabaseHas('users', $validData);
     }
 }
