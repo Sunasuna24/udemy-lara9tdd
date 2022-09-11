f719288b8c5acb2aa58a8af72c8442ba83da19e5
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-11 09:51:01 +0900

    会員登録画面用のrouteを追加した

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SignUpController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index']);
+
+Route::get('/signup', [SignUpController::class, 'index']);
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show')->whereNumber('post');
