8f63fde0a5a7dd1ce084f6269308a65460dd2e68
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-13 00:38:56 +0900

    PostManageのコントローラとテストの雛形を作成した

---
+++ tests/Feature/Http/Controllers/MyPage/PostManageControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\MyPage;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostManageControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
