4b0687c268b19d1e070b1a963cef25730b8d823e
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-13 01:51:43 +0900

    自分の投稿のみをマイページに表示させた

--- app/Http/Controllers/MyPage/PostManageController.php
+++ app/Http/Controllers/MyPage/PostManageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\MyPage;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostManageController extends Controller
 {
     public function index()
     {
-        return view('mypage.posts.index');
+        $posts = Post::where('user_id', Auth::id())->get();
+        return view('mypage.posts.index', compact('posts'));
     }
 }
--- resources/views/mypage/posts/index.blade.php
+++ resources/views/mypage/posts/index.blade.php
@@ -4,4 +4,24 @@
 <h1>マイブログ一覧</h1>
 <a href="/mypage/post/create">ブログ新規登録</a>
 <hr>
+<table>
+    <tr>
+        <th>ブログ名</th>
+    </tr>
+    @foreach ($posts as $post)
+    <tr>
+        <td>{{ $post->title }}</td>
+        {{-- <td>
+            <a href="{{ route('mypage.post.edit') }}">{{ $post->title }}</a>
+        </td>
+        <td>
+            <form action="{{ route('mypage.post.delete', $post->id) }}" method="post">
+                @csrf
+                @method('delete')
+                <input type="submit" value="削除">
+            </form>
+        </td> --}}
+    </tr>
+    @endforeach
+</table>
 @endsection
